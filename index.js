b300b2d9e6241a136973a5ad941db860b19ccf13
Diogo Cezar <diogo@diogocezar.com>
2018-04-29 10:01:12 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const graphql     = require('graphql');
+const graphqlHTTP = require('express-graphql');
+const express     = require('express');
+const users       = require('./schema/userSchema');
+const app         = express();
+
+app.use('/user', graphqlHTTP({ schema: users, pretty: true }))
+
+app.listen(3000, function () {
+    console.log('Server on.')
+})
